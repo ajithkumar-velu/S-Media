64cdc90377c312e915e678c018dd96a9007a55ca
Ajith Kumar V <ajithajk46@gmail.com>
2025-02-12 19:41:44 +0530

    Delete node_modules directory

--- node_modules/react-modal-pro/src/lib/utils/checkHash.ts
+++
@@ -1,5 +0,0 @@
-export const checkHash = (modalKey: string) => {
-    const currentHash = window.location.hash;
-    const isAlreadyInHash = currentHash.split("#").some((item) => item === modalKey);
-    return { isAlreadyInHash, currentHash };
-};
